bab3471fee7647852a2324417485530258e8de89
236709 Ziemecki Szymon <236709@edu.p.lodz.pl>
2021-03-11 10:10:17 +0100

    test

---
+++ ziemecki/introduction/program/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello World!";
+    int i=factorial(5);
+    cout<<i<<endl;
+    return 0;
+}
