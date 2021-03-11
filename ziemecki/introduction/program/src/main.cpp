e2404b9af59f4c6b805cd906b65e6757f73837b9
236709 Ziemecki Szymon <236709@edu.p.lodz.pl>
2021-03-11 10:41:37 +0100

    OSTATNI

--- ziemecki/introduction/program/src/main.cpp
+++ ziemecki/introduction/program/src/main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << "Co jest" << endl;
+    cout << "Ostatni test" << endl;
     int i=factorial(5);
     cout<<i<<endl;
+
     return 0;
 }
