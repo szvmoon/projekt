55e17541d091595a7ca085d11ee86c58a6831812
236709 Ziemecki Szymon <236709@edu.p.lodz.pl>
2021-03-11 10:32:34 +0100

    test2

--- ziemecki/introduction/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/atom_sr_1200_07/ziemecki/introduction
-# Build directory: /home/student/CLionProjects/atom_sr_1200_07/ziemecki/introduction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
