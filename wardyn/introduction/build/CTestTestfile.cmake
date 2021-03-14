48687d9c491da76ac28a25e80b934593f8aa2c7b
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-14 22:16:39 +0100

    introduction

--- wardyn/introduction/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/introduction
-# Build directory: /home/student/introduction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
