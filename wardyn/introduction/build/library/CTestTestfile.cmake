561c61569cc61946788c8ba18a7d53f66dc2dd93
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-14 22:14:32 +0100

    introduction

---
+++ wardyn/introduction/build/library/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/introduction/library
+# Build directory: /home/student/introduction/build/library
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ wardyn/introduction/program/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main ()
+{
+    cout << "hello world!";
+    cout << endl;
+
+    int b=factorial(5);
+    cout << b << endl;
+    return 0;
+}
