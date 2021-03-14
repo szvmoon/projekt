9a93ae82c2c9f840c558d8058f9c557b576757e5
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-14 22:02:49 +0100

    introduction

---
+++ Wardyn/Introduction/build/library/CMakeFiles/MathLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o"
+  "libMathLibrary.pdb"
+  "libMathLibrary.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Wardyn/Introduction/library/include/math_helpers.h
@@ -0,0 +1,5 @@
+#ifndef MATH_HELPERS_H
+#define MATH_HELPERS_H  
+
+unsigned long long factorial(unsigned int n);
+#endif
