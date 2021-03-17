6af3bdf2a5b8526716494fff9e5f4931a44fcda0
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-17 13:46:45 +0100

    tset

---
+++ wardyn/workshop/CarRental/cmake-build-debug/library/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental/library
+# Build directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental/cmake-build-debug/library
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ wardyn/workshop/CarRental/cmake-build-debug/program/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/src/main.cpp.o"
+  "Program"
+  "Program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
