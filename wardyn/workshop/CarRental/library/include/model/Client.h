83a2ffd780e7a16cd0754a3f928d2ea7eedd3cf6
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-17 16:37:50 +0100

    x

---
+++ wardyn/workshop/CarRental/library/include/model/Client.h
@@ -0,0 +1,14 @@
+//
+// Created by student on 17.03.2021.
+//
+
+#ifndef CARRENTALPROJECT_CLIENT_H
+#define CARRENTALPROJECT_CLIENT_H
+
+
+class Client {
+
+};
+
+
+#endif //CARRENTALPROJECT_CLIENT_H
