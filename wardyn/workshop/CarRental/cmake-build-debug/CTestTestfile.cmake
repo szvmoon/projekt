78f81ec3c47bb93ed2548e26efee066c15cfa708
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-17 13:48:29 +0100

    mem

--- wardyn/workshop/CarRental/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental
-# Build directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
--- wardyn/workshop/CarRental/cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental/library
-# Build directory: /home/student/atom_sr_1200_07/wardyn/workshop/CarRental/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
