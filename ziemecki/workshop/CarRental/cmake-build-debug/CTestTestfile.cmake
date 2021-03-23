e38e52194e5e90f570af93c2598e18ee5369fd2d
236709 Ziemecki Szymon <236709@edu.p.lodz.pl>
2021-03-23 16:09:21 +0100

    usuwacja

--- ziemecki/workshop/CarRental/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/atom_sr_1200_07/ziemecki/workshop/CarRental
-# Build directory: /home/student/CLionProjects/atom_sr_1200_07/ziemecki/workshop/CarRental/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
