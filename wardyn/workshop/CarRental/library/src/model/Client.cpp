7808d0cefa2d94242f2f6d09cdbc803a9883e3a5
236683 Wardyn Jakub <236683@edu.p.lodz.pl>
2021-03-23 18:40:09 +0100

    carrental

---
+++ wardyn/workshop/CarRental/library/src/model/Client.cpp
@@ -0,0 +1,30 @@
+//
+// Created by student on 17.03.2021.
+//
+
+#include "model/Client.h"
+#include <iostream>
+
+using namespace std;
+
+Client::Client(string firstName, string lastName, string personalID) :firstName(firstName), lastName(lastName), personalID(personalID) {}
+
+Client::~Client() {}
+
+string Client::getClientInfo () const
+{
+    return ("Client, "+firstName+", "+lastName+", "+personalID+"\n");
+}
+
+void Client::setClientInfo (string name, string surname)
+{
+    if (name != "")
+    {
+        firstName = name;
+    }
+    if (surname!="")
+    {
+        lastName = surname;
+    }
+}
+
